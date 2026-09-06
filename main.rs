bbbd829cf5397d2107441622d7d5d9f0682d0876
Michal Pokorný <michal.pokorny0@gmail.com>
2026-09-06 15:49:56 +0200

    13

--- main.rs
+++ main.rs
@@ -7,16 +7,24 @@ use std::collections::HashMap;
 fn main() {
     let stdin = std::io::stdin();
     let mut it = stdin.lock().lines();
-    let s = it.next().unwrap().unwrap();
+    //let s = it.next().unwrap().unwrap();
     //let n: i32 = it.next().unwrap().unwrap().trim().parse().unwrap();
-    let nums: Vec<i64> = s.split_whitespace()
-        .map(|s| s.parse().unwrap())
+    let nums: Vec<_> = it.map(|s| s.unwrap().trim().parse::<i32>().unwrap())
         .collect();
     
-    let max = nums.iter()
-    .filter(|n| *n % 2 == 0)
-    .map(|n| n * n)
-    .sum::<i64>();
+    let a = Point { x: nums[0], y: nums[1] };
+    let b = Point { x: nums[2], y: nums[3] };
 
-    println!("{}", max);
+    println!("{}", a.dist(&b));
 }
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn dist(&self, other: &Point) -> i32 {
+        (other.x - self.x).pow(2) + (other.y - self.y).pow(2)
+    }
+} 
