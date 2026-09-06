e1f3d9c5a5ff407ac649bd4abe367a9220925479
Michal Pokorný <michal.pokorny0@gmail.com>
2026-09-06 15:14:37 +0200

    09

--- main.rs
+++ main.rs
@@ -6,11 +6,12 @@ use std::io::{BufRead};
 fn main() {
     let stdin = std::io::stdin();
     let mut it = stdin.lock().lines();
-    let n: i32 = it.next().unwrap().unwrap().trim().parse().unwrap();
+    let s = it.next().unwrap().unwrap();
+    //let n: i32 = it.next().unwrap().unwrap().trim().parse().unwrap();
 
-    println!("{}", s(n));
+    println!("{}", count(&s));
 }
 
-fn s(n: i32) -> i64 {
-    n as i64 * n as i64
+fn count(s: &str) -> usize {
+    s.len()
 }
