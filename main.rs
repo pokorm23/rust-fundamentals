b0dc8948f7c00ba67800cce4ae1fb02857ac1c65
Michal Pokorný <michal.pokorny0@gmail.com>
2026-09-06 15:58:23 +0200

    14

--- main.rs
+++ main.rs
@@ -4,27 +4,40 @@
 use std::io::{BufRead};
 use std::collections::HashMap;
 
+use crate::Light::{Green, Red, Yellow};
+
 fn main() {
     let stdin = std::io::stdin();
     let mut it = stdin.lock().lines();
-    //let s = it.next().unwrap().unwrap();
+    let s = it.next().unwrap().unwrap();
     //let n: i32 = it.next().unwrap().unwrap().trim().parse().unwrap();
-    let nums: Vec<_> = it.map(|s| s.unwrap().trim().parse::<i32>().unwrap())
-        .collect();
+    //let nums: Vec<_> = it.map(|s| s.unwrap().trim().parse::<i32>().unwrap())
+    //    .collect();
+
+    let c = match s.trim() {
+        "red" => Red,
+        "yellow" => Yellow,
+        _ => Green
+    };
     
-    let a = Point { x: nums[0], y: nums[1] };
-    let b = Point { x: nums[2], y: nums[3] };
-
-    println!("{}", a.dist(&b));
+    println!("{}", name(&next(c)));
 }
 
-struct Point {
-    x: i32,
-    y: i32,
+enum Light {
+    Red, Yellow, Green
 }
 
-impl Point {
-    fn dist(&self, other: &Point) -> i32 {
-        (other.x - self.x).pow(2) + (other.y - self.y).pow(2)
+fn next(l: Light) -> Light {
+    match l {
+        Light::Red => Light::Green,
+        Light::Green => Light::Yellow,
+        Light::Yellow => Light::Red,
     }
-} 
+}
+fn name(l: &Light) -> &str {
+    match l {
+        Light::Red => "red",
+        Light::Yellow => "yellow",
+        Light::Green => "green",
+    }
+}
