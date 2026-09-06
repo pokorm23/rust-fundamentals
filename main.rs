48844ba4d8cc2cfb72dca1b1e1671b7659a83467
Michal Pokorný <michal.pokorny0@gmail.com>
2026-09-06 15:37:50 +0200

    12

--- main.rs
+++ main.rs
@@ -2,20 +2,21 @@
 #![warn(clippy::nursery)]
 
 use std::io::{BufRead};
-use std::collections::{HashSet};
+use std::collections::HashMap;
 
 fn main() {
     let stdin = std::io::stdin();
     let mut it = stdin.lock().lines();
     let s = it.next().unwrap().unwrap();
     //let n: i32 = it.next().unwrap().unwrap().trim().parse().unwrap();
-    let mut seen: HashSet<&str> = HashSet::new();
+    let nums: Vec<i64> = s.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
     
-    for n in s.split_whitespace() {
-        seen.insert(n);
-    }
+    let max = nums.iter()
+    .filter(|n| *n % 2 == 0)
+    .map(|n| n * n)
+    .sum::<i64>();
 
-    let d = seen.len();
-
-    println!("{}", d);
+    println!("{}", max);
 }
