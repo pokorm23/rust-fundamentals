f1c302f67ea1de777be420630441963e03889e9f
Michal Pokorný <michal.pokorny0@gmail.com>
2026-09-06 15:05:21 +0200

    08

--- main.rs
+++ main.rs
@@ -8,11 +8,9 @@ fn main() {
     let mut it = stdin.lock().lines();
     let n: i32 = it.next().unwrap().unwrap().trim().parse().unwrap();
 
-    let mut t: i64 = 0;
-    for i in 1..=n {
-        t += i as i64;
-    }
-
-    println!("{}", t);
+    println!("{}", s(n));
 }
- 
+
+fn s(n: i32) -> i64 {
+    n as i64 * n as i64
+}
