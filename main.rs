e2f24a7594bfe78323061998ce108767ead61bc0
Michal Pokorný <michal.pokorny0@gmail.com>
2026-09-06 14:04:56 +0200

    04

--- main.rs
+++ main.rs
@@ -4,8 +4,5 @@ fn main() {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).unwrap();
 
-    let mut it = input.split_whitespace();
-    let w: i64 = it.next().unwrap().parse().unwrap();
-    let h: i64 = it.next().unwrap().parse().unwrap();
-    println!("{}", w * h);
+    println!("{}", input.to_uppercase());
 }
